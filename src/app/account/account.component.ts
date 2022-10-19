90c1250fe853ed2115f358abe30e2efa82ae2581
Yujie Wang <langyuyang@outlook.com>
2022-10-19 20:50:24 +0800

    cross-components communication

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,19 +1,16 @@
 import { Component, Input } from '@angular/core';
 import { AccountsService } from '../accoounts.service';
-// import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css'],
-  // providers: [LoggingService]
 })
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
 
   constructor(
-    // private loggingService: LoggingService,
     private accountService: AccountsService
   ) {
 
@@ -21,6 +18,6 @@ export class AccountComponent {
 
   onSetTo(status: string) {
     this.accountService.updateAccount(this.id, status);
-    // this.loggingService.logStatusChange(status)
+    this.accountService.statusUpdated.emit(status);
   }
 }
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,24 +1,22 @@
 import { Component } from '@angular/core';
 import { AccountsService } from '../accoounts.service';
-// import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  // providers: [LoggingService]
 })
 
 export class NewAccountComponent {
   constructor(
-    // // private loggingService: LoggingService,
     private accountService: AccountsService
   ) {
-
+    this.accountService.statusUpdated.subscribe(
+      (status: string) => alert('new status: ' + status)
+    );
   }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountService.addAccount(accountName, accountStatus);
-    // this.loggingService.logStatusChange(accountStatus);
   }
 }
