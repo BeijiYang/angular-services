46467b97057e018d75ab21e12364f23d692a61db
Yujie Wang <langyuyang@outlook.com>
2022-10-19 20:10:04 +0800

    create & inject accounts service

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { AccountsService } from '../accoounts.service';
 import { LoggingService } from '../logging.service';
 
 @Component({
@@ -10,15 +11,16 @@ import { LoggingService } from '../logging.service';
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
-  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
-  constructor(private loggingService: LoggingService) {
+  constructor(
+    private loggingService: LoggingService,
+    private accountService: AccountsService
+  ) {
 
   }
 
   onSetTo(status: string) {
-    this.statusChanged.emit({id: this.id, newStatus: status});
+    this.accountService.updateAccount(this.id, status);
     this.loggingService.logStatusChange(status)
-    // console.log('A server status changed, new status: ' + status);
   }
 }
